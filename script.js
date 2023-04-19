4f4bf46319203fd51237212fce768986a98c2af8
elaaasri <anas_elasri@outlook.fr>
2023-04-19 18:04:13 +0000

    v2 completed

--- script.js
+++ script.js
@@ -1,81 +1,113 @@
-    // selecting elements :
-let numberButtons = document.querySelectorAll(".number-buttons");
-let operatorButtons = document.querySelectorAll(".operator-buttons");
-let displayArea = document.querySelector(".display");
-let solutionArea = document.querySelector(".solution");
-
-    // funcs for basic math operators : 
-const add = (a,b) => a + b;
-const subtract = (a,b) => a - b;
-const multiply = (a,b) => a * b;
-const divide = (a,b) => a / b;
-
-    // declaring variables : 
-let storeFirstNumber = "";
-let storeOperator = "";
-let storeSecondNumber = "";
-let result = "";
-let clickCount = 0;
-
-    // func to calcule 2 numbers and shows the result : 
-function operate() {
-
-    let operator = storeOperator;
-    a = Number(storeFirstNumber);
-    b = Number(storeSecondNumber);
-
-    switch (operator) {
-        case "+":
-            solutionArea.textContent = add(a,b)
-            break;
-        case "-":
-            solutionArea.textContent = subtract(a,b)
-            break;
-        case "*":
-            solutionArea.textContent = multiply(a, b);
-            break;
-        case "/":
-            solutionArea.textContent = divide(a, b);
-            break;
-        default: 
-            displayArea.textContent = "no operator found!";
-        };
+        // selecting elements :
+    let numberButtons = document.querySelectorAll(".number-buttons");
+    let operatorButtons = document.querySelectorAll(".operator-buttons");
+    let displayArea = document.querySelector(".display");
+    let solutionArea = document.querySelector(".solution");
+    let clearButton = document.querySelector(".clear-button");
+    
+        // funcs for basic math operators : 
+    const add = (a,b) => a + b;
+    const subtract = (a,b) => a - b;
+    const multiply = (a,b) => a * b;
+    const divide = (a,b) => a / b;
+    
+        // declaring variables :
+    let displayValue = "";
+    let storeFirstNumber = "";
+    let storeOperator = "";
+    let storeSecondNumber = "";
+    let clickCount = 0;
+    let result = "";
+    
+        // func to calcule 2 numbers and shows the result : 
+    function operate() {
+    
+        let operator = storeOperator;
+        a = Number(storeFirstNumber);
+        b = Number(storeSecondNumber);
+        
+        switch (operator) {
+            case "+":
+                solutionArea.textContent = add(a,b)
+                break;
+            case "-":
+                solutionArea.textContent = subtract(a,b)
+                break;
+            case "*":
+                solutionArea.textContent = multiply(a, b);
+                break;
+            case "/":
+                solutionArea.textContent = divide(a, b);
+                break;
+            };
     };
 
-    // func to show nums : 
-function showNumbers() {
-    
-    numberButtons.forEach(numBtn => {
-        numBtn.addEventListener("click",
-        () => {
-            displayArea.textContent += numBtn.textContent;
-            storeSecondNumber = displayArea.textContent.slice(1);
+        // func to show nums : 
+    function showNumbers() {
+        
+        numberButtons.forEach(numBtn => {
+            numBtn.addEventListener("click",
+            () => {
+                displayArea.textContent += numBtn.textContent;
+                displayValue += numBtn.textContent;
+                storeSecondNumber = displayArea.textContent.slice(1);
+            });
         });
-    });
-};
-showNumbers();
+    };
+    showNumbers();
+    
+        // func to show operators and calls operate() : 
+    function showOperators() { 
+        operatorButtons.forEach(oprBtn => { 
+            oprBtn.addEventListener("click", () => {
 
-    // func to show operators and calls operate() : 
-function showOperators() { 
-    operatorButtons.forEach(oprBtn => { 
-        oprBtn.addEventListener("click", 
-        () => {
-            
-            if(clickCount === 0) {
+                    // fix the bug if "=" entered first : 
+                if(clickCount === 0 && oprBtn.textContent === "=") {
+                    solutionArea.textContent = "0";
+                    return;
+                };
+
+                    // storing nums based on clickcount : 
+                if(clickCount === 0) {
+                    storeFirstNumber = displayValue;
                     clickCount++;
                 } else if (clickCount === 1) {
                     operate();
                     clickCount++;
-                } 
-                else if (clickCount === 2) {
+                } else if (clickCount === 2) {
                     storeFirstNumber = solutionArea.textContent;
                     operate();
                 };
+                
+                displayArea.textContent = oprBtn.textContent;
+                storeOperator = oprBtn.textContent;
 
-            storeFirstNumber = parseInt(displayArea.textContent);
-            displayArea.textContent = oprBtn.textContent;
-            storeOperator = oprBtn.textContent;
+                    // calls show snarky message func :
+                showSnarkyMessage();
+
+            });
         });
-    });
-};
-showOperators();
+    };
+    showOperators();
+
+        // func to show a snarky message if the user divide by 0! :
+    function showSnarkyMessage() {
+        result = solutionArea.textContent;
+            if (result === "Infinity") {
+            alert("her we go again :)))")
+            deleteData();
+            };
+    };
+
+        // func to delete all data : 
+    function deleteData() {
+        displayValue = "";
+        storeFirstNumber = "";
+        storeOperator = "";
+        storeSecondNumber = "";
+        displayArea.textContent = "";
+        solutionArea.textContent = "";
+        clickCount = 0;
+    };
+
+    clearButton.addEventListener("click", deleteData);
